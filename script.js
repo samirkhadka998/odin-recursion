0b23cebf91bf6e68f6e04ae15717413c774ce74f
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-11-23 08:48:09 +0545

    Recursion Fibonacci

--- script.js
+++ script.js
@@ -19,4 +19,37 @@ function fibs(num) {
   
     return arr;
   }
+
+ //Recursion Fibonacci
+
+ console.log(fibsRec(8));
+ function fibsRec(num, arr = []) {
+    if (num <= 0)
+      return [];
+    if (num == 1) {
+      return [0]
+    }
+    if (num == 2) {
+      return [0, 1];
+    }
+  
+    if (arr.length == num) {
+      return arr;
+    }
+  
+    if (arr.length == 0) {
+      arr.push(0);
+      arr.push(1);
+    }
+  
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  
+    let returnArr = fibsRec(num, arr);
+    if(returnArr.length === num){
+      return arr
+    }
+  
+  }
+  
+  
   
