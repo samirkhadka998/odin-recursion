2a0f3b7d7d70b07a9af0675822e76312a0e87360
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-11-23 08:48:59 +0545

    Merge sort algorithm implemented

--- script.js
+++ script.js
@@ -50,6 +50,40 @@ function fibs(num) {
     }
   
   }
+
+
+//merge sort 
+
+let arr = [3,2,1];
+
+console.log(mergeSort(arr))
+
+function mergeSort(arr) {
+  if(arr.length == 1){
+    return arr;
+  }
+  let left = [];
+  let right = [];
+  let mid = arr.length/2;
+  left = arr.slice(0, mid);
+  right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const sortedArr = [];
+  while(left.length > 0 && right.length > 0){
+     if(left[0] <= right [0]){
+        sortedArr.push(left.shift())
+     }
+     else{
+      sortedArr.push(right.shift())
+     }
+  }
+
+  return [...sortedArr, ...left, ...right]
+}
   
   
   
